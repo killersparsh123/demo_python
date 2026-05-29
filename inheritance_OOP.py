2eb6e9035f9696455774447b104745bf3a9ac307
stuti pangeni <stutipangeni@sparsh-macbook.local>
2026-05-29 18:52:18 +0545

    inheritance OOP (some feature are added from earlier func)

---
+++ inheritance_OOP.py
@@ -0,0 +1,12 @@
+class animal:
+    def speak(self):
+        print("animal speaks")
+class dog(animal):
+    def barks(self):
+        print("dogs barks ")
+Dog=dog()
+Dog.speak()
+Dog.barks()
+
+
+
